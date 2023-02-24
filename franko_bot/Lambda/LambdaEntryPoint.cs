fcdaeddf86cce23820035f898560d56069416df1
lolpie244 <lolpie2651@gmail.com>
2023-02-25 00:50:01 +0200

    removed Lambda deploy

--- franko_bot/Lambda/LambdaEntryPoint.cs
+++
@@ -1,8 +0,0 @@
-namespace franko_bot;
-
-public class LambdaEntryPoint : Amazon.Lambda.AspNetCoreServer.APIGatewayProxyFunction {
-	protected override void Init(IWebHostBuilder builder)
-	{
-		builder.UseStartup<Startup>();
-	}
-}
